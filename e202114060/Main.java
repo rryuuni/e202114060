63203056cf79539e5512873dd792a91284a838a5
rryuuni <fkdbs1122@naver.com>
2024-09-25 09:38:18 +0900

    third

--- e202114060/Main.java
+++ e202114060/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) {
-      System.out.println("양라윤");
-    }
-  
+  public static void main(String[] args) {
+    System.out.println("양라윤");
+    World.run();
   }
-  
+
+}
